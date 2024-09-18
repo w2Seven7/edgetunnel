baf066e492711667c02c7e60c387cc2800c17b72
GGBoom <134898272+w2Seven7@users.noreply.github.com>
2024-09-18 16:43:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1651,7 +1651,7 @@ function subAddresses(host,UUID,noTLS,newAddressesapi,newAddressescsv,newAddress
 		if(proxyhosts.length > 0 && (伪装域名.includes('.workers.dev') || 伪装域名.includes('pages.dev'))) {
 			最终路径 = `/${伪装域名}${最终路径}`;
 			伪装域名 = proxyhosts[Math.floor(Math.random() * proxyhosts.length)];
-			节点备注 = ` 【一叶知秋】`;
+			节点备注 = ` 【恭喜发财】`;
 		}
 		
 		const 协议类型 = atob(啥啥啥_写的这是啥啊);
